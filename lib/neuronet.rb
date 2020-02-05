f281be240c569f9d73e7e7cce3c1eea50fb2c76a
carlosjhr64 <carlosjhr64@gmail.com>
2020-02-05 12:44:57 -0800

    Vishnu and Shiva

--- lib/neuronet.rb
+++ lib/neuronet.rb
@@ -576,5 +576,57 @@ module Neuronet
     end
   end
 
+  # Vishnu is a network which has its @yang layer initially mirror and "shadow" @yin.
+  module Vishnu
+    # Vishnu.bless sets the weights of pairing even yang (@yang[2*i], @yin[i]) connections to WONE,
+    # and pairing odd yang (@yang[2*i+1], @yin[i]) connections to negative WONE.
+    # Likewise the bias with BZERO.
+    # This makes @yang initially mirror and shadow @yin.
+    # The pairing is done starting with (@yang[0], @yin[0]).
+    # That is, starting with (@yang.first, @yin.first).
+    def self.bless(myself)
+      yang = myself.yang
+      # just cover as much as you can
+      yin_length = [myself.yin.length, yang.length/2].min
+      # connections from yang[2*i] to yin[i] are WONE... mirroring to start.
+      # connections from yang[2*i+1] to yin[i] are -WONE... shadowing to start.
+      0.upto(yin_length-1) do |index|
+        even = yang[2*index]
+        odd = yang[(2*index)+1]
+        even.connections[index].weight = WONE
+        even.bias = BZERO
+        odd.connections[index].weight  = -WONE
+        odd.bias = -BZERO
+      end
+      return myself
+    end
+  end
+
+  # Shiva is a network which has its @out layer initially mirror and "shadow" @yang.
+  module Shiva
+    # Shiva.bless sets the weights of pairing even out (@out[2*i], @yang[i]) connections to WONE,
+    # and pairing odd out (@out[2*i+1], @yang[i]) connections to negative WONE.
+    # Likewise the bias with BZERO.
+    # This makes @out initially mirror and shadow @yang.
+    # The pairing is done starting with (@out[0], @yang[0]).
+    # That is, starting with (@out.first, @yang.first).
+    def self.bless(myself)
+      out = myself.out
+      # just cover as much as you can
+      yang_length = [myself.yang.length, out.length/2].min
+      # connections from out[2*i] to yang[i] are WONE... mirroring to start.
+      # connections from out[2*i+1] to yang[i] are -WONE... shadowing to start.
+      0.upto(yang_length-1) do |index|
+        even = out[2*index]
+        odd = out[(2*index)+1]
+        even.connections[index].weight = WONE
+        even.bias = BZERO
+        odd.connections[index].weight  = -WONE
+        odd.bias = -BZERO
+      end
+      return myself
+    end
+  end
+
   # TODO: Combos?
 end
