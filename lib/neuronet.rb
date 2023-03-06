92dbc3332b3bb02ebb6d9658efea84df03ce202b
CarlosJHR64 <carlosjhr64@gmail.com>
2023-03-06 13:09:40 -0800

    Build bump

--- lib/neuronet.rb
+++ lib/neuronet.rb
@@ -2,7 +2,7 @@
 
 # Neuronet is a neural network library for Ruby.
 module Neuronet
-  VERSION = '7.0.230305'
+  VERSION = '7.0.230306'
   require_relative           'neuronet/constants'
   autoload :Connection,      'neuronet/connection'
   autoload :Neuron,          'neuronet/neuron'
